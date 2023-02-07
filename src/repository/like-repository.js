ac9cbc1cd56c76c6254150a27c8b082474d31057
arungupta7221 <48250441+arungupta7221@users.noreply.github.com>
2023-02-07 17:58:52 +0530

    added crud repo and extended in like-repo

---
+++ src/repository/like-repository.js
@@ -0,0 +1,8 @@
+import CrudRepository from './crud-repository.js'
+import Like from '../models/like.js'
+class LikeRepository extends CrudRepository {
+  constructor() {
+    super(Like)
+  }
+}
+export default CrudRepository
